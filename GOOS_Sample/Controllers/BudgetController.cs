1306c5b1d97aa76b565d52e962aef0b4540164de
JANET\JanetWang <ching.miou@gmail.com>
2017-07-01 15:26:35 +0800

    Step8:完成Controller,Service未完成

--- GOOS_Sample/Controllers/BudgetController.cs
+++ GOOS_Sample/Controllers/BudgetController.cs
@@ -4,12 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using GOOS_Sample.Models.DataModels;
+using GOOS_Sample.Models;
 
 namespace GOOS_Sample.Controllers
 {
     public class BudgetController : Controller
     {
+        private IBudgetService budgetService = new BudgetService();
+
+        public BudgetController(IBudgetService budgetService)
+        {
+            this.budgetService = budgetService;
+        }
+
+        public BudgetController()
+        {
+            
+        }
         // GET: Budget
         public ActionResult Add()
         {
@@ -19,12 +30,8 @@ namespace GOOS_Sample.Controllers
         [HttpPost]
         public ActionResult Add(BudgetAddViewModel model)
         {
-            using (var dbcontext = new GOOS_Sample.Models.DataModels.Aluxe_TestEntities())
-            {
-                var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
-                dbcontext.Budgets.Add(budget);
-                dbcontext.SaveChanges();
-            }
+
+            this.budgetService.Create(model);
             ViewBag.Message = "added successfully";
             return View(model);
         }
---
+++ GOOS_Sample/Models/BudgetService.cs
@@ -0,0 +1,18 @@
+﻿using GOOS_Sample.Models.DataModels;
+using GOOS_Sample.Models.ViewModels;
+
+namespace GOOS_Sample.Models
+{
+    public class BudgetService : IBudgetService
+    {
+        public void Create(BudgetAddViewModel model)
+        {
+            using (var dbcontext = new GOOS_Sample.Models.DataModels.Aluxe_TestEntities())
+            {
+                var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
+                dbcontext.Budgets.Add(budget);
+                dbcontext.SaveChanges();
+            }
+        }
+    }
+}
